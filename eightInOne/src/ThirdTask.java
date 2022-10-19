f306ea774454c8ea3bd47dc87bd5451c7fb1a945
dadadaniil <115701903+dadadaniil@users.noreply.github.com>
2022-10-19 19:45:46 +0300

    Add files via upload

--- eightInOne/src/ThirdTask.java
+++ eightInOne/src/ThirdTask.java
@@ -1,10 +1,17 @@
 public class ThirdTask {
     public static void main(String[] args) {
+
         double a, b, c;
         double res;
         a = 1;
         b = a;
         c = b;
+
+        if (a == 0) {
+            System.out.println("impossible to divide by zero");
+            return;
+        }
         res = (b + Math.sqrt(b * b + 4 * a * c)) / 2 * a - a * a * a * c + 1 / b * b;
+
     }
 }
